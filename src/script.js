455fe7e77c589850ce98314c924b64b8cd999fa6
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-06 23:05:45 +0100

    finished

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import earthVertexShader from './shaders/earth/vertex.glsl'
 import earthFragmentShader from './shaders/earth/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphere/vertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphere/fragment.glsl'
 
 /**
  * Base
@@ -40,9 +42,15 @@ gui.addColor(earthParameters, 'atmosphereDayColor').onChange(() => {
   earthMaterial.uniforms.uAtmosphereDayColor.value.set(
     earthParameters.atmosphereDayColor
   )
+  atmosphereMaterial.uniforms.uAtmosphereDayColor.value.set(
+    earthParameters.atmosphereDayColor
+  )
 })
 gui.addColor(earthParameters, 'atmosphereTwilightColor').onChange(() => {
   earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(
+    earthParameters.atmosphereTwilightColor
+  )
+  atmosphereMaterial.uniforms.uAtmosphereTwilightColor.value.set(
     earthParameters.atmosphereTwilightColor
   )
 })
@@ -85,6 +93,18 @@ scene.add(earth)
 
 // Atmosphere
 const atmosphereMaterial = new THREE.ShaderMaterial({
+  vertexShader: atmosphereVertexShader,
+  fragmentShader: atmosphereFragmentShader,
+  uniforms: {
+    uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+    uCloudIntensity: new THREE.Uniform(earthParameters.cloudIntensity),
+    uAtmosphereDayColor: new THREE.Uniform(
+      new THREE.Color(earthParameters.atmosphereDayColor)
+    ),
+    uAtmosphereTwilightColor: new THREE.Uniform(
+      new THREE.Color(earthParameters.atmosphereTwilightColor)
+    ),
+  },
   side: THREE.BackSide,
   transparent: true,
 })
@@ -115,6 +135,7 @@ const updateSun = () => {
 
   // Uniforms
   earthMaterial.uniforms.uSunDirection.value.copy(sunDirection)
+  atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection)
 }
 updateSun()
 
