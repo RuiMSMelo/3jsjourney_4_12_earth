22458ad23014fc95de99167b30e26721de7025e7
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-06 22:07:52 +0100

    day/night added

--- src/script.js
+++ src/script.js
@@ -22,51 +22,102 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Earth
  */
+// Textures
+const earthDayTexture = textureLoader.load('./earth/day.jpg')
+earthDayTexture.colorSpace = THREE.SRGBColorSpace
+earthDayTexture.anisotropy = 8
+
+const earthNightTexture = textureLoader.load('./earth/night.jpg')
+earthNightTexture.colorSpace = THREE.SRGBColorSpace
+earthNightTexture.anisotropy = 8
+
+const earthSpecularCloudsTexture = textureLoader.load(
+  './earth/specularClouds.jpg'
+)
+earthSpecularCloudsTexture.anisotropy = 8
+
 // Mesh
 const earthGeometry = new THREE.SphereGeometry(2, 64, 64)
 const earthMaterial = new THREE.ShaderMaterial({
-    vertexShader: earthVertexShader,
-    fragmentShader: earthFragmentShader,
-    uniforms:
-    {
-    }
+  vertexShader: earthVertexShader,
+  fragmentShader: earthFragmentShader,
+  uniforms: {
+    uDayTexture: new THREE.Uniform(earthDayTexture),
+    uNightTexture: new THREE.Uniform(earthNightTexture),
+    uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture),
+    uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+  },
 })
 const earth = new THREE.Mesh(earthGeometry, earthMaterial)
 scene.add(earth)
 
 /**
+ * Sun
+ */
+const sunSpherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5)
+const sunDirection = new THREE.Vector3()
+
+// Debug
+const debugSun = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(debugSun)
+
+// Update
+const updateSun = () => {
+  // Sun direction
+  sunDirection.setFromSpherical(sunSpherical)
+
+  //Debug
+  debugSun.position.copy(sunDirection).multiplyScalar(5)
+
+  // Uniforms
+  earthMaterial.uniforms.uSunDirection.value.copy(sunDirection)
+}
+updateSun()
+
+// Tweaks
+gui.add(sunSpherical, 'phi').min(0).max(Math.PI).onChange(updateSun)
+gui.add(sunSpherical, 'theta').min(-Math.PI).max(Math.PI).onChange(updateSun)
+
+/**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    pixelRatio: Math.min(window.devicePixelRatio, 2)
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 12
-camera.position.y = 5
-camera.position.z = 4
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 20
+camera.position.y = 10
+camera.position.z = 10
 scene.add(camera)
 
 // Controls
@@ -77,8 +128,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
@@ -89,20 +140,19 @@ renderer.setClearColor('#000011')
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    earth.rotation.y = elapsedTime * 0.1
+  earth.rotation.y = elapsedTime * 0.1
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -1,3 +1,8 @@
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+uniform vec3 uSunDirection;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -6,7 +11,19 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    float sunOrientation = dot(uSunDirection, normal);
+
+    // Day/night color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
+
+    // Specular clouds color
+    vec2 specularCloudsColor = texture(uSpecularCloudsTexture, vUv.rg);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
